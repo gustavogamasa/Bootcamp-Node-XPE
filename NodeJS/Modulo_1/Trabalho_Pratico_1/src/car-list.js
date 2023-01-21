b43b5739c3c0bb7785e10f9b7b2aa923f7b018df
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2023-01-21 16:20:54 -0300

    Tests new

---
+++ NodeJS/Modulo_1/Trabalho_Pratico_1/src/car-list.js
@@ -0,0 +1,57 @@
+import {promises as fs} from "fs";
+
+let dados = readFile();
+
+
+export async function maisModelos() {
+
+    const maiorQtd = Math.max(...dados.map(item => {
+        return item.models.length;
+    }));
+
+    // Encontrar marca com essa quantidade
+    const marcaMaiorQtd = dados.filter(item => item.models.length === maiorQtd)
+
+    let result;
+
+    // Se houver empate
+    if (marcaMaiorQtd.length > 1) {
+
+        result = res.json(marcaMaiorQtd.map(item => {
+            return ({
+                Marca_com_mais_modelos: item.brand,
+                Quantidade_Modelos: item.models.length
+            });
+        }))
+
+    } // Se não houver empate
+    else {
+
+        result = res.json({
+            Marca_com_mais_modelos: marcaMaiorQtd[0].brand,
+            Quantidade_Modelos: marcaMaiorQtd[0].models.length
+        })
+
+    }
+
+
+    return result;
+
+}
+
+
+
+
+
+
+
+
+
+
+async function readFile() {
+
+    const dados = await fs.readFile("./car-list.json");
+    console.log(" ----- Carregou car-list.json ------");
+
+    return dados;
+}
