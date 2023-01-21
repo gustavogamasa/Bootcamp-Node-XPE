da4cccdd3bce5d59c246c2a14f05022ad46b7cf6
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2023-01-21 13:00:11 -0300

    Teste ok

--- NodeJS/Modulo_1/Teste_2/index.js
+++ NodeJS/Modulo_1/Teste_2/index.js
@@ -1,9 +1,46 @@
 import express, { json } from "express";
+import {promises as fs} from "fs";
 
 const app = express();
 app.use(express.json())
 
-app.get("/status", (req, res) => {return res.json({status: "ok"})})
+
+
+app.get("/file", async (req, res) => {
+    
+    let dados = await readFile();
+    // console.log(JSON.parse(dados));
+    return res.json(JSON.parse(dados))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function readFile(){
+
+    const dados =  await fs.readFile("./car-list.json");
+
+    return dados;
+}
+
+
+
+
+
+
 
 
 
